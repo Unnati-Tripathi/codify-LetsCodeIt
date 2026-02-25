0424c08693ba8c07aaa0aedac815bb45b5f8211a
Unnati-Tripathi <unnatitripathi670@gmail.com>
2026-02-25 08:39:37 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -49,5 +49,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
